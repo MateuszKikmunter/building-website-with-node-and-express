dad826a24cf1e2036724a0abe24a8557cba3b8d0
m.kikmunter <M.Kikmunter@gmail.com>
2020-01-21 14:58:18 +0000

    ignoring requests for favicon.ico

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,10 @@ const app = express();
 const routes = require("./routes");
 
 app.use(express.static("./assets"));
+app.get("/favicon.ico", (req, rest, next) => {
+    return rest.sendStatus(204);
+});
+
 app.use("/", routes());
 app.listen(3000);
 
