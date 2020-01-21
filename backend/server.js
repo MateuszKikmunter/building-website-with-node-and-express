5e0ebd84d564c56f04736d7d42a1b0555086c1d8
m.kikmunter <M.Kikmunter@gmail.com>
2020-01-21 15:53:51 +0000

    added 404 route and view

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const createError = require("http-errors");
 const path = require("path");
 const app = express();
 
@@ -13,6 +14,21 @@ app.get("/favicon.ico", (req, rest, next) => {
 });
 
 app.use("/", routes());
+
+//add 404 route handler
+app.use((req, res, next) => {
+    return next(createError(404, "File not found"));
+});
+
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    const status = err.status || 500;
+    res.locals.status = status;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+    res.status(status);
+    return res.render("not-found");
+});
+
 app.listen(3000);
 
 module.export = app;
